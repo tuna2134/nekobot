ed86a41974496d958f3c6cdf3790f1e207812f0f
コマリン親衛隊 <contact@tuna2134.dev>
2024-02-23 14:07:42 +0000

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import { useState } from 'react';
-import style from './styles/main.module.scss';
+import { useState } from "react";
+import style from "./styles/main.module.scss";
+import { useForm } from "react-hook-form";
 
 interface ImageType {
   uri: string;
   nowLoading: boolean;
 }
 
+enum InputImageType {
+  Neko = "neko",
+}
+
+interface Inputs {
+  imageType: InputImageType;
+}
+
+const ENDPOINT = "https://nekobot.xyz/api/image";
+
 function App() {
   const [image, setImage] = useState<ImageType>({
     uri: "",
     nowLoading: true,
   });
+  const { register, handleSubmit } = useForm<Inputs>();
+  /*
   const handleClick = () => {
     setImage({...image, nowLoading: false})
     fetch("https://nekobot.xyz/api/image?type=neko")
@@ -20,18 +33,39 @@ function App() {
         })
       })
   }
+  */
+
+  const onSubmit = (inputs: Inputs) => {
+    setImage({ ...image, nowLoading: false });
+    fetch(`${ENDPOINT}?type=${inputs.imageType}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setImage({ nowLoading: true, uri: data.message });
+      });
+  };
 
   return (
     <div className={style.root}>
       <div>
         <h1 className={style.title}>Nekobot imaging</h1>
-        <div className={style.outsideButton}>
-          <button className={style.button} onClick={handleClick}>Click</button>
-        </div>
-        {image.nowLoading ? <img src={image.uri} className={style.img} /> : <p>Now loading</p>}
+        <form onSubmit={handleSubmit(onSubmit)} className={style.outsideButton}>
+          <select {...register("imageType")}>
+            {Object.entries(InputImageType).map(([key, value], index) => (
+              <option value={value} key={index}>
+                {key}
+              </option>
+            ))}
+          </select>
+          <button className={style.button}>Show me</button>
+        </form>
+        {image.nowLoading ? (
+          <img src={image.uri} className={style.img} />
+        ) : (
+          <p>Now loading</p>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
