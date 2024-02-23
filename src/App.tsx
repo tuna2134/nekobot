2104a046bcd8c9d0a64bbbcd2c5ec5feeab766b6
masato.04.11.2007 <masato.04.11.2007@gmail.com>
2024-02-23 11:21:36 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import style from './main.module.scss';
+
+interface ImageType {
+  uri: string;
+  nowLoading: boolean;
+}
+
+function App() {
+  const [image, setImage] = useState<ImageType>({
+    uri: "",
+    nowLoading: true,
+  });
+  const handleClick = () => {
+    setImage({...image, nowLoading: false})
+    fetch("https://nekobot.xyz/api/image?type=neko")
+      .then((res: Response) => {
+        res.json().then((data) => {
+          setImage({nowLoading: true, uri: data.message})
+        })
+      })
+  }
+
+  return (
+    <div className={style.root}>
+      <div>
+        <h1 className={style.title}>Nekobot imaging</h1>
+        <div className={style.outsideButton}>
+          <button className={style.button} onClick={handleClick}>Click</button>
+        </div>
+        {image.nowLoading ? <img src={image.uri} className={style.img} /> : <p>Now loading</p>}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.module.scss.d.ts
@@ -0,0 +1,5 @@
+export declare const button: string;
+export declare const img: string;
+export declare const outsideButton: string;
+export declare const root: string;
+export declare const title: string;
